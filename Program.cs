5d28e72f37d5b0fbc3dc5bc79a65b2cc3abae562
WpFeltac <hberleur@gmail.com>
2021-03-28 23:10:16 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace HeadQuarters
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("HeadQuarters v" + Assembly.GetEntryAssembly().GetName().Version.ToString());
+        }
+    }
+}
